3c3af92cf646ddf2432e5591c9c08ea68a340f93
Orkun Karaduman <ok@orkunkaraduman.com>
2023-09-22 01:19:13 +0300

    refactored by new Handler

--- rapi.go
+++ rapi.go
@@ -5,27 +5,44 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"reflect"
 	"strconv"
 
 	"github.com/goinsane/logng"
 )
 
-type HandleFunc func(in interface{}, send SendFunc)
+type DoFunc func(in interface{}, send SendFunc)
 type SendFunc func(out interface{}, code int)
 
 type Handler struct {
 	Logger *logng.Logger
+	In     interface{}
+	Do     DoFunc
 }
 
-func (h *Handler) Handle(w http.ResponseWriter, r *http.Request, in interface{}, fn HandleFunc) {
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	defer func(Body io.ReadCloser) {
 		_ = Body.Close()
 	}(r.Body)
 
+	inVal := reflect.ValueOf(h.In)
+	isInValPtr := inVal.Kind() == reflect.Pointer
+	var indirectInVal reflect.Value
+	if !isInValPtr {
+		indirectInVal = inVal
+	} else {
+		indirectInVal = inVal.Elem()
+	}
+	if indirectInVal.Kind() != reflect.Struct {
+		panic("input must be struct or struct pointer")
+	}
+	copiedInVal := reflect.New(indirectInVal.Type())
+	copiedInVal.Elem().Set(indirectInVal)
+
 	send := func(out interface{}, code int) {
 		data, err := json.Marshal(out)
 		if err != nil {
-			h.Logger.Errorf("unable to marshal json: %w", err)
+			h.Logger.Errorf("unable to marshal response body to json: %w", err)
 			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 			return
 		}
@@ -42,19 +59,24 @@ func (h *Handler) Handle(w http.ResponseWriter, r *http.Request, in interface{},
 
 	data, err := io.ReadAll(r.Body)
 	if err != nil {
-		h.Logger.Errorf("unable to read data: %w", err)
-		send(&errorResponse{Error: "read data error"}, http.StatusBadRequest)
+		h.Logger.Errorf("unable to read request body: %w", err)
+		send(&errorResponse{Error: "unable to read request body"}, http.StatusBadRequest)
 		return
 	}
 
-	p := makePtr(in)
-
-	err = json.Unmarshal(data, p)
+	err = json.Unmarshal(data, copiedInVal.Interface())
 	if err != nil {
-		h.Logger.Errorf("unable to unmarshal json: %w", err)
-		send(&errorResponse{Error: "json unmarshal error"}, http.StatusBadRequest)
+		h.Logger.Errorf("unable to unmarshal request body from json: %w", err)
+		send(&errorResponse{Error: "unable to unmarshal request body from json"}, http.StatusBadRequest)
 		return
 	}
 
-	fn(p, send)
+	var in interface{}
+	if !isInValPtr {
+		in = copiedInVal.Elem().Interface()
+	} else {
+		in = copiedInVal.Interface()
+	}
+
+	h.Do(in, send)
 }
