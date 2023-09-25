a3f0996b696e8fe4756d9f2c6eff029660f2433a
Orkun Karaduman <ok@orkunkaraduman.com>
2023-09-25 20:17:18 +0300

    changed arg name in DoFunc type

--- rapi.go
+++ rapi.go
@@ -2,5 +2,5 @@ package rapi
 
 import "net/http"
 
-type DoFunc func(req *http.Request, in interface{}, header http.Header, send SendFunc)
+type DoFunc func(req *http.Request, in interface{}, respHeader http.Header, send SendFunc)
 type SendFunc func(out interface{}, code int)
