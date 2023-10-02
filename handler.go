8b685f14e6f6a0d03235def2f1f8dc89cd341a76
Orkun Karaduman <ok@orkunkaraduman.com>
2023-10-03 00:12:02 +0300

    dont parse non-struct inputs on get, head methods

--- handler.go
+++ handler.go
@@ -137,11 +137,13 @@ func (h *methodHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	copiedInVal := copyReflectValue(inVal)
 
 	if contentType == "" && (r.Method == http.MethodHead || r.Method == http.MethodGet) {
-		err = valuesToStruct(r.URL.Query(), copiedInVal.Interface())
-		if err != nil {
-			h.options.PerformError(fmt.Errorf("invalid query: %w", err), r)
-			httpError(r, w, "invalid query", http.StatusBadRequest)
-			return
+		if copiedInVal.Elem().Kind() == reflect.Struct {
+			err = valuesToStruct(r.URL.Query(), copiedInVal.Interface())
+			if err != nil {
+				h.options.PerformError(fmt.Errorf("invalid query: %w", err), r)
+				httpError(r, w, "invalid query", http.StatusBadRequest)
+				return
+			}
 		}
 	} else {
 		var rd io.Reader = r.Body
