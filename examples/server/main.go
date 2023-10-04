78f7c4108fabe97d457ec761a562fb7e4b439374
Orkun Karaduman <ok@orkunkaraduman.com>
2023-10-04 14:00:32 +0300

    improved server example, added echo endpoint

--- examples/server/main.go
+++ examples/server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
@@ -16,23 +15,28 @@ func main() {
 		log.Print(err)
 	}))
 
-	unimplemented := func(req *rapi.Request, header http.Header, send rapi.SendFunc) {
-		fmt.Println(req.In)
-		send(&messages.ErrorResponse{
-			ErrorMsg: http.StatusText(http.StatusNotImplemented),
-		}, http.StatusNotImplemented)
-	}
-
 	handler.Handle("/").
-		Register("", nil, unimplemented)
+		Register("", nil,
+			func(req *rapi.Request, header http.Header, send rapi.SendFunc) {
+				send(&messages.ErrorResponse{
+					ErrorMsg: http.StatusText(http.StatusNotImplemented),
+				}, http.StatusNotImplemented)
+			})
 
-	handler.Handle("/ping").Register("GET", new(messages.PingRequest),
-		func(req *rapi.Request, respHeader http.Header, send rapi.SendFunc) {
-			in := req.In.(*messages.PingRequest)
-			send(&messages.PingReply{
-				Payload: in.Payload,
-			}, http.StatusOK)
-		})
+	handler.Handle("/ping").
+		Register("GET", new(messages.PingRequest),
+			func(req *rapi.Request, respHeader http.Header, send rapi.SendFunc) {
+				in := req.In.(*messages.PingRequest)
+				send(&messages.PingReply{
+					Payload: in.Payload,
+				}, http.StatusOK)
+			})
+
+	handler.Handle("/echo").
+		Register("POST", nil,
+			func(req *rapi.Request, respHeader http.Header, send rapi.SendFunc) {
+				send(req.In, http.StatusOK)
+			})
 
 	err = http.ListenAndServe(":8080", handler)
 	if err != nil {
